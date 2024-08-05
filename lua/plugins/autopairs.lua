f6d7e3c4b82b21d4d2b6bbe76eb11cd4f1dc186f
peter <peter@peterdeMacBook-Pro.local>
2024-08-05 14:27:36 +0800

    增加 wildrie插件，默认选中最接近“ { 的内容

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,12 +1,16 @@
 return {
-	"windwp/nvim-autopairs",
-	event = "InsertEnter",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		dependencies = {
+			"windwp/nvim-ts-autotag",
+		},
+		config = function()
+			require("nvim-autopairs").setup({})
+			-- 启用对 HTML 文件的自动闭合
+			require("nvim-ts-autotag").setup()
+		end,
 	},
-	config = function()
-		require("nvim-autopairs").setup({})
-		-- 启用对 HTML 文件的自动闭合
-    require('nvim-ts-autotag').setup()
-	end,
+	-- 可以最快选中 ' " { (  的内容
+	{ "gcmt/wildfire.vim" },
 }
