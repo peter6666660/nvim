6df19c0e93076e82ca17af43b373930f78a25684
peter <peter@peterdeMacBook-Pro.local>
2024-08-22 23:55:19 +0800

    lsp: 手动配置，并增加vue lsp - volar

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,40 +1,49 @@
 local status, lspconfig = pcall(require, "lspconfig")
 if not status then
-  return
+	return
 end
 
 local M = {}
 
 function M.lua_ls()
-  lspconfig.lua_ls.setup({
-    settings = {
-      Lua = {
-        diagnostics = {
-          -- 识别 `vim` 是全局变量，避免报错
-          globals = { "vim" },
-        },
-        workspace = {
-          -- 使语言服务器识别 Neovim 运行时文件
-          library = vim.api.nvim_get_runtime_file("", true),
-          -- 对第三方插件和 Neovim 内部文件进行支持
-          checkThirdParty = false,
-        },
-        -- 禁用遥测数据
-        telemetry = {
-          enable = false,
-        },
-      },
-    },
-  })
+	lspconfig.lua_ls.setup({
+		settings = {
+			Lua = {
+				diagnostics = {
+					-- 识别 `vim` 是全局变量，避免报错
+					globals = { "vim" },
+				},
+				workspace = {
+					-- 使语言服务器识别 Neovim 运行时文件
+					library = vim.api.nvim_get_runtime_file("", true),
+					-- 对第三方插件和 Neovim 内部文件进行支持
+					checkThirdParty = false,
+				},
+				-- 禁用遥测数据
+				telemetry = {
+					enable = false,
+				},
+			},
+		},
+	})
 end
 
 function M.tsserver()
-  lspconfig.tsserver.setup {}
+	lspconfig.tsserver.setup({})
+end
+
+function M.volar()
+	lspconfig.volar.setup({
+		flags = {
+			debounce_text_changes = 150,
+		},
+	})
 end
 
 function M.setup(lsp)
-  M.lua_ls()
-  M.tsserver()
+	M.lua_ls()
+	M.tsserver()
+  M.volar()
 end
 
 return M
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -48,11 +48,11 @@ return {
       require("mason").setup({})
       require("mason-lspconfig").setup({
         ensure_installed = { "lua_ls", "tsserver", "cssls" },
-        handlers = {
-          function(server_name)
-            require("lspconfig")[server_name].setup({})
-          end,
-        },
+        -- handlers = {
+        --   function(server_name)
+        --     require("lspconfig")[server_name].setup({})
+        --   end,
+        -- },
       })
 
       -- 自定义lsp
