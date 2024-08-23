91e85d71c1b8574bb83b6c1606b04e47c28166c6
peter <peter@peterdeMacBook-Pro.local>
2024-08-23 15:14:39 +0800

    增加最低版本控制- 0.10.1

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
+-- 检查 Neovim 版本是否至少为 x.xx.x
+local function check_minimum_version(major, minor, patch)
+    local version = vim.version()
+    if version.major < major or (version.major == major and version.minor < minor) or (version.major == major and version.minor == minor and version.patch < patch) then
+        vim.api.nvim_err_writeln("Neovim version " .. major .. "." .. minor .. "." .. patch .. " or higher is required!")
+        vim.cmd("quit")
+    end
+end
+
+-- 设定最低版本为 0.10.1
+check_minimum_version(0, 10, 1)
+
 require("config.lazy")
 
