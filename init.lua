b9d4c92f796fdc1ce0dd42e3b7341a9f87957690
peter <peter@peterdeMacBook-Pro.local>
2024-08-23 15:43:40 +0800

    版本控制增加延时退出

--- init.lua
+++ init.lua
@@ -1,14 +1,22 @@
 -- 检查 Neovim 版本是否至少为 x.xx.x
 local function check_minimum_version(major, minor, patch)
-    local version = vim.version()
-    if version.major < major or (version.major == major and version.minor < minor) or (version.major == major and version.minor == minor and version.patch < patch) then
-        vim.api.nvim_err_writeln("Neovim version " .. major .. "." .. minor .. "." .. patch .. " or higher is required!")
-        vim.cmd("quit")
-    end
+	local version = vim.version()
+	if
+		version.major < major
+		or (version.major == major and version.minor < minor)
+		or (version.major == major and version.minor == minor and version.patch < patch)
+	then
+		vim.api.nvim_err_writeln(
+			"Neovim version " .. major .. "." .. minor .. "." .. patch .. " or higher is required!"
+		)
+
+		vim.defer_fn(function()
+			vim.cmd("quit")
+		end, 1000)
+	else
+		require("config.lazy")
+	end
 end
 
 -- 设定最低版本为 0.10.1
 check_minimum_version(0, 10, 1)
-
-require("config.lazy")
-
