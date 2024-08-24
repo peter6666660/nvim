57a6f31aa29498cb8bcd4d5a9c5b801533960284
peter <peter@peterdeMacBook-Pro.local>
2024-08-24 08:55:01 +0800

    删除了lsp_attach 一堆代码

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,32 +14,13 @@ return {
 			{ "hrsh7th/cmp-nvim-lsp" },
 			{ "williamboman/mason.nvim" },
 			{ "WhoIsSethDaniel/mason-tool-installer.nvim" }, -- 自动加载一些插件
-			{ "williamboman/mason-lspconfig.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" }, -- 简化lsp的配置
 		},
 		config = function()
 			local lsp_zero = require("lsp-zero")
 
-			-- lspsaga 替代
-			-- lsp_attach is where you enable features that only work
-			-- if there is a language server active in the file
-			-- local lsp_attach = function(client, bufnr)
-			-- local opts = { buffer = bufnr, noremap = true, silent = true }
-			-- lsp_zero.buffer_autoformat()
-			-- vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-			-- vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-			-- vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-			-- vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-			-- vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-			-- vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-			-- vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-			-- vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-			-- vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-			-- vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-			-- end
-
 			lsp_zero.extend_lspconfig({
 				sign_text = true,
-				-- lsp_attach = lsp_attach,
 				capabilities = require("cmp_nvim_lsp").default_capabilities(),
 			})
 
