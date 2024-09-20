af9347a808f64f9a2c310d2810b4852296b28c97
peter <peter@peterdeMacBook-Pro.local>
2024-09-20 09:38:32 +0800

    修改neovide透明度

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,5 @@ vim.opt.shiftwidth = 2
 -- vim.opt.foldenable = false
 -- vim.opt.foldmethod = "expr"
 -- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+vim.g.neovide_transparency = 0.8 -- 设置透明度为 80%
