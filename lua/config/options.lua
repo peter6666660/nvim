951b7dbbb931b407e27d26264f9f2e030f68603a
peter <peter6666660@gmail.com>
2024-09-23 09:15:11 +0800

    更新折叠层级显示

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,11 +6,11 @@ vim.opt.shiftwidth = 2
 vim.g.neovide_transparency = 0.8 -- 设置透明度为 80%
 
 -- 配置折叠方式
-vim.o.foldenable = true
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.o.foldcolumn = "0" -- '0' is not bad
-vim.opt.signcolumn = "yes" -- 始终显示符号列
+vim.o.foldenable = true -- 开启折叠
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+vim.o.foldcolumn = "2" -- '0' is not bad  显示折叠符号
+vim.o.signcolumn = "yes" -- 始终显示符号列
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 
