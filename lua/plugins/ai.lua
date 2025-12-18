8f901f55f3aae642addce64813f331701bf433bc
Parker <parker@MacBook-Pro.local>
2025-12-18 09:36:14 +0800

    hidden ai/avante

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -4,34 +4,34 @@ return {
 		event = "BufEnter",
 	},
 	-- -- INFO: Chart
-	{
-		"yetone/avante.nvim",
-		-- build = vim.fn.has("win32") ~= 0
-		-- 		and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-		-- 	or "make",
-		event = "VeryLazy",
-		version = false,
-		opts = {
-			provider = "ollama",
-			providers = {
-				ollama = {
-					-- Ollama 服务地址（完整带端口）
-					endpoint = "http://192.168.0.14:11434",
-					model = "qwen2.5-coder:14b",
-
-					-- 可选：把这些放到 extra_request_body
-					extra_request_body = {
-						temperature = 0.2,
-					},
-
-					-- 如果需要工具执行，可能需要这些
-					timeout = 60000, -- 60s
-				},
-			},
-		},
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-		},
-	},
+	-- {
+	-- 	"yetone/avante.nvim",
+	-- 	-- build = vim.fn.has("win32") ~= 0
+	-- 	-- 		and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+	-- 	-- 	or "make",
+	-- 	event = "VeryLazy",
+	-- 	version = false,
+	-- 	opts = {
+	-- 		provider = "ollama",
+	-- 		providers = {
+	-- 			ollama = {
+	-- 				-- Ollama 服务地址（完整带端口）
+	-- 				endpoint = "http://192.168.0.14:11434",
+	-- 				model = "qwen2.5-coder:14b",
+	--
+	-- 				-- 可选：把这些放到 extra_request_body
+	-- 				extra_request_body = {
+	-- 					temperature = 0.2,
+	-- 				},
+	--
+	-- 				-- 如果需要工具执行，可能需要这些
+	-- 				timeout = 60000, -- 60s
+	-- 			},
+	-- 		},
+	-- 	},
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"MunifTanjim/nui.nvim",
+	-- 	},
+	-- },
 }
